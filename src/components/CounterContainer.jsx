454978564a1c4db78bd0291e7551eee70c5d6501
cyriacinfo <cyriac.intesse@gmail.com>
2021-12-13 12:33:03 +0100

    coucou

--- src/components/CounterContainer.jsx
+++ src/components/CounterContainer.jsx
@@ -10,6 +10,10 @@ const CountComponent = ({ number, dispatch }) => (
     <p>{number}</p>
     <button type="button" onClick={() => dispatch({ type: 'INCREMENT' })}>+1</button>
     <button type="button" onClick={() => dispatch({ type: 'DECREMENT' })}>-1</button>
+    <button type="button" onClick={() => dispatch({ type: 'INCREMENT10' })}>+10</button>
+    <button type="button" onClick={() => dispatch({ type: 'DECREMENT10' })}>-10</button>
+    <button type="button" onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
+
   </div>
 );
 export default connect(mapCountToProps)(CountComponent);
